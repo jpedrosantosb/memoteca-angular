0c21e609091499c96d40a330d4b2de274e735913
jpedrosantosb <jpedrosantosb@gmail.com>
2023-06-20 22:14:30 -0300

    pesquisando pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,17 +17,26 @@ export class ListarPensamentoComponent implements OnInit {
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.panginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.panginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     });
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.panginaAtual).subscribe(listaPensamentos =>{
+    this.service.listar(++this.panginaAtual, this.filtro).subscribe(listaPensamentos =>{
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length){
         this.haMaisPensamentos = false
       }
     })
+  }
+
+  pesquisarPensamentos() {
+    this.haMaisPensamentos = true
+    this.panginaAtual = 1
+    this.service.listar(this.panginaAtual, this.filtro)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos = listaPensamentos
+      })
   }
 }
